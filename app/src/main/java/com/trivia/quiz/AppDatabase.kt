468c539abd87aa793b35a07cf2ce15257c0fb53c
Alireza Rafee <alirezarafee@Alirezas-Mac-mini.local>
2021-08-05 17:56:47 +0430

    Add room and tirivia database

---
+++ app/src/main/java/com/trivia/quiz/AppDatabase.kt
@@ -0,0 +1,9 @@
+package com.trivia.quiz
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+
+@Database(entities = [Question::class], version = 1)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun questionDao(): QuestionDao
+}
--- app/src/main/java/com/trivia/quiz/MainActivity.kt
+++ app/src/main/java/com/trivia/quiz/MainActivity.kt
@@ -2,10 +2,35 @@ package com.trivia.quiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.lifecycle.lifecycleScope
+import androidx.room.Room
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        lifecycleScope.launch(Dispatchers.IO) {
+
+            try {
+
+            val db = Room.databaseBuilder(this@MainActivity, AppDatabase::class.java, "TestDB3.db")
+                .createFromAsset("myapp.db")
+                .build()
+//        val db = Room.databaseBuilder(
+//            applicationContext,
+//            AppDatabase::class.java, "database-name"
+//        ).build()
+
+            val questions = db.questionDao().getAll()
+            Log.i("questions_tag", "onCreate: ${questions.toString()}")
+            } catch (e : Exception){
+                Log.i("db_error", "onCreate: ${e.message}")
+            }
+        }
     }
 }
